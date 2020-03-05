96cf9daa0ce6997142150c64c1f0e43d8708e36c
Joaquin4562 <martinez.hernandez.17087@itsmante.edu.mx>
2020-03-05 07:33:37 -0600

    05/03/2020

---
+++ lib/widgets/widgets_register_page/txtRegistrarte.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class TxtRegistrate extends StatefulWidget {
+  @override
+  _TxtRegistrateState createState() => _TxtRegistrateState();
+}
+
+class _TxtRegistrateState extends State<TxtRegistrate> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 60,),
+      child: RotatedBox(
+          quarterTurns: 0,
+          child: Text(
+            'Registrate',
+            style: TextStyle(
+              letterSpacing: 7,
+              color: Colors.white,
+              fontSize: 38,
+              fontWeight: FontWeight.w900,
+            ),
+          )),
+    );
+  }
+}
