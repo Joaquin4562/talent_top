247b897bea439edd7efb4ba8107943e9682e5938
Joaquin4562 <martinez.hernandez.17087@itsmante.edu.mx>
2020-03-06 14:02:07 -0600

    ei

--- lib/pages/horarios_page.dart
+++ lib/pages/horarios_page.dart
@@ -19,55 +19,64 @@ class _HorariosPageState extends State<HorariosPage> {
           children: <Widget>[
             Column(
               children: <Widget>[
-                HeadTextHorarios(color: colorFondo,),
+                HeadTextHorarios(
+                  color: colorFondo,
+                ),
                 BotonElige()
-                ],
+              ],
             ),
-                Column(
-                  children: <Widget>[
-                    Container(
-                      height: 600,
-                      width: double.infinity,
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(0),
-                            topRight: Radius.circular(100)),
-                      ),
-                      child: ListView(
+            Column(
+              children: <Widget>[
+                Container(
+                  height: 600,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(0),
+                        topRight: Radius.circular(100)),
+                  ),
+                  child: ListView(
+                    children: <Widget>[
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: <Widget>[
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Card(
-                                margin: EdgeInsets.only(left: 20),
-                                elevation: 10,
-                                child: Container(
-                                  height: 50,
-                                  width: 380,
-                                  child: InkWell(
-                                    onTap: (){},
-                                    child: Row(
-                                      children: <Widget>[
-                                        Expanded(
-                                          child: Text('Curso x',
-                                          style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold),),
+                          Text(
+                            'LUNES',
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.bold),
+                          ),
+                          Card(
+                              margin: EdgeInsets.only(left: 20),
+                              elevation: 10,
+                              child: Container(
+                                height: 50,
+                                width: 380,
+                                child: InkWell(
+                                  onTap: () {},
+                                  child: Row(
+                                    children: <Widget>[
+                                      Expanded(
+                                        child: Text(
+                                          'Curso x',
+                                          style: TextStyle(
+                                              fontSize: 25,
+                                              fontWeight: FontWeight.bold),
                                         ),
-                                        Icon(Icons.arrow_forward_ios)
-                                      ],
-                                    ),
+                                      ),
+                                      Icon(Icons.arrow_forward_ios)
+                                    ],
                                   ),
-                                )
-                              )
-                            ],
-                          )
+                                ),
+                              ))
                         ],
-                      ),
-                    ),
-                  ],
-                )
+                      )
+                    ],
+                  ),
+                ),
               ],
             )
-        );
+          ],
+        ));
   }
 }
