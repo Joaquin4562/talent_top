03da3b481d53a3e86b985b00760a1b3b40159b01
KTDRAL <angel.catedral.1410@gmail.com>
2020-03-11 17:03:35 -0600

    Recuperacion del registro v1.0.7 ahora sí

---
+++ lib/class/cursos.dart
@@ -0,0 +1,17 @@
+class Cursos {
+  String hora;
+  String autor;
+  String nombre;
+  String dia;
+
+  Cursos({this.autor, this.hora, this.nombre, this.dia});
+}
+
+class curso {
+  
+  String hora;
+  String autor;
+  String nombre;
+  String dia;
+
+}
